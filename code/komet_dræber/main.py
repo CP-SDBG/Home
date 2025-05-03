4c2c7ae21c2bc7c2c36bb45601cb1cd3f62785f7
Henrik Pankoke <hpankoke@ige-xao.com>
2025-05-03 14:24:13 +0200

    Update main.py

--- code/komet_dræber/main.py
+++ code/komet_dræber/main.py
@@ -12,6 +12,12 @@ ship.angle = 0
 ship.vx = 0
 ship.vy = 0
 
+# Skud
+bullets = []
+
+# Kometer
+meteors= []
+
 # Stjerner
 stars = []
 for _ in range(200):
@@ -19,8 +25,12 @@ for _ in range(200):
         "x": random.randint(-WIDTH, WIDTH),
         "y": random.randint(-HEIGHT, HEIGHT),
         "size": random.choice([1, 2])
-
     })
+
+# Game
+score = 0
+lives = 3
+game_over = False
 
 def draw():
     # Rens vinduet
@@ -33,7 +43,28 @@ def draw():
     # Tegn Spiller
     ship.draw()
 
+    # Tegn skud
+    for b in bullets:
+        b.draw()
+
+    # Tegn Kometer
+    for m in meteors:
+        m.draw()
+
+    # Game
+    if game_over:
+        screen.draw.text("GAME OVER", center=(WIDTH/2, HEIGHT/2), fontsize=60, color="red", owidth=1.0)
+        screen.draw.text("Tryk R for at genstarte", center=(WIDTH/2, HEIGHT/2+50), fontsize=40, color="white")
+    else:
+        screen.draw.text(f"Point: {score}", (10, 10), fontsize=40, color="white")
+        screen.draw.text(f"Lives: {lives}", (10,50), fontsize=40, color="white")
+
+
 def update():
+    global game_over
+    if game_over:
+        return
+
     # Roter spiller
     if keyboard.left:
         ship.angle += 4
@@ -62,3 +93,100 @@ def update():
     if ship.y > HEIGHT:
         ship.y = 0
 
+    # Kollision mellem skud og komet
+    for b in bullets[:]:
+        for m in meteors[:]:
+            if b.colliderect(m):
+                dx = m.x - b.x
+                dy = m.y - b.y
+                distance = math.hypot(dx, dy)
+                if distance <= (m.width/2):
+                    global score
+                    score += 1
+                    bullets.remove(b)
+                    meteors.remove(m)
+                    break
+
+    # Kollision med komet og spiller
+    for m in meteors:
+        if m.colliderect(ship):
+            dx = ship.x - m.x
+            dy = ship.y - m.y
+            distance = math.hypot(dx, dy)
+            if distance <= (m.width/2):
+                meteors.remove(m)
+                global lives
+                lives -= 1
+                if lives < 1:
+                    game_over = True
+                else:
+                    ship.pos = WIDTH/2,HEIGHT/2
+
+                break
+
+    # Skud
+    for b in bullets:
+        b.x += b.vx
+        b.y += b.vy
+    for b in bullets:
+        if not (0 <= b.x <= WIDTH and 0 <= b.y <= HEIGHT):
+            bullets.remove(b)
+
+    # Spawn kometer
+    if random.random() < 0.03:
+        spawn_meteor()
+
+    # Kometerne
+    for m in meteors:
+        m.x += m.vx
+        m.y += m.vy
+        if not (0 <= m.x <= WIDTH and 0 <= m.y <= HEIGHT):
+            meteors.remove(m)
+
+def on_key_down(key):
+    # Afgiv skud
+    if key == keys.SPACE:
+        bullet = Actor('bullet')
+        bullet.pos = ship.pos
+        bullet.angle = ship.angle
+        angle_rad = math.radians(bullet.angle)
+        bullet.vx = -math.sin(angle_rad) * 8
+        bullet.vy = -math.cos(angle_rad) * 8
+        bullets.append(bullet)
+    elif key == keys.R and game_over:
+        reset_game()
+
+def spawn_meteor():
+    size = random.choice(["meteor_small", "meteor_large"])
+    meteor = Actor(size)
+
+    start_side= random.choice(['top', 'bottom', 'left', 'right'])
+    if start_side == 'top':
+        meteor.x = random.randint(0, WIDTH)
+        meteor.y = 0
+    elif start_side == 'bottom':
+        meteor.x = random.randint(0, WIDTH)
+        meteor.y = HEIGHT
+    elif start_side == 'left':
+        meteor.x = 0
+        meteor.y = random.randint(0, HEIGHT)
+    elif start_side == 'right':
+        meteor.x = WIDTH
+        meteor.y = random.randint(0, HEIGHT)
+
+    meteor.vx = random.uniform(-1.5, 1.5)
+    meteor.vy = random.uniform(1, 3)
+
+    meteors.append(meteor)
+
+def reset_game():
+    global game_over, score, lives, meteors, bullets
+    game_over = False
+    score = 0
+    lives = 3
+    meteors = []
+    bullets = []
+    ship.pos = (WIDTH/2, HEIGHT/2)
+    ship.angle = 0
+    ship.vx = 0
+    ship.vy = 0
